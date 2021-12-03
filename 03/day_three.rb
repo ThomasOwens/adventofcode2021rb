fd66144c85d8f64e6d611a2c5906c03694523f54
Thomas J. Owens <thomas.j.owens@gmail.com>
2021-12-03 16:11:24 -0500

    Day 3 Part 2 Answered

--- 03/day_three.rb
+++ 03/day_three.rb
@@ -103,47 +103,28 @@
 
 class DiagnosticReport
   def initialize
-    @report = {}
+    @entries = []
   end
 
   def parse_entry(entry)
-    entry.chars.each_with_index do |value, index|
-      if @report.key?(index)
-        if @report[index].key?(value.to_i)
-          @report[index][value.to_i] += 1
-        else
-          @report[index][value.to_i] = 1
-        end
-      else
-        @report[index] = {}
-        @report[index][value.to_i] = 1
-      end
-    end
+    @entries.push(entry)
   end
 
   def gamma_rate
-    most_common_bits = Array.new(@report.keys.length)
+    most_common_bits = Array.new(bit_analysis(@entries).keys.length)
 
-    @report.keys.each_with_index do |_, index|
-      most_common_bits[index] = if @report[index][0] > @report[index][1]
-                                  0
-                                else
-                                  1
-                                end
+    bit_analysis(@entries).keys.each_with_index do |_, index|
+      most_common_bits[index] = most_common_bit(@entries, index)
     end
 
     most_common_bits.join.to_i(2)
   end
 
   def epsilon_rate
-    least_common_bits = Array.new(@report.keys.length)
+    least_common_bits = Array.new(bit_analysis(@entries).keys.length)
 
-    @report.keys.each_with_index do |_, index|
-      least_common_bits[index] = if @report[index][0] < @report[index][1]
-                                   0
-                                 else
-                                   1
-                                 end
+    bit_analysis(@entries).keys.each_with_index do |_, index|
+      least_common_bits[index] = least_common_bit(@entries, index)
     end
 
     least_common_bits.join.to_i(2)
@@ -151,7 +132,82 @@ class DiagnosticReport
 
   def power_consumption
     gamma_rate * epsilon_rate
-  end    
+  end
+
+  def oxygen_generator_rating
+    entries = @entries.clone
+    
+    current_index = 0
+
+    while entries.length != 1
+      keep_bit = if most_common_bit?(entries, current_index)
+                   most_common_bit(entries, current_index).to_s
+                 else
+                   '1'
+                 end
+
+      entries.delete_if { |element| element[current_index] != keep_bit }
+
+      current_index += 1
+    end
+
+    entries[0].to_i(2)
+  end
+
+  def co2_scrubber_rating
+    entries = @entries.clone
+    
+    current_index = 0
+
+    while entries.length != 1
+      keep_bit = if most_common_bit?(entries, current_index)
+                   least_common_bit(entries, current_index).to_s
+                 else
+                   '0'
+                 end
+
+      entries.delete_if { |element| element[current_index] != keep_bit }
+
+      current_index += 1
+    end
+
+    entries[0].to_i(2)
+  end
+
+  private
+
+    def bit_analysis(entries)
+      bit_analysis = {}
+
+      entries.each do |entry| 
+        entry.chars.each_with_index do |value, index|
+          if bit_analysis.key?(index)
+            if bit_analysis[index].key?(value.to_i)
+              bit_analysis[index][value.to_i] += 1
+            else
+              bit_analysis[index][value.to_i] = 1
+            end
+          else
+            bit_analysis[index] = {}
+            bit_analysis[index][value.to_i] = 1
+          end
+        end
+      end
+
+      bit_analysis
+    end
+
+    def most_common_bit(entries, index)
+      bit_analysis(entries)[index].key(bit_analysis(entries)[index].values.max)
+    end
+
+    def least_common_bit(entries, index)
+      bit_analysis(entries)[index].key(bit_analysis(entries)[index].values.min)
+    end
+
+    def most_common_bit?(entries, index)
+      most_common_bit(entries, index) != least_common_bit(entries, index)
+    end
 end
 
 if $PROGRAM_NAME == __FILE__
@@ -161,4 +217,6 @@ if $PROGRAM_NAME == __FILE__
   File.foreach(File.new(ARGV[0])) { |line| diagnostic_report.parse_entry(line.chomp) }
   
   puts "Part 1: Power Consumption = #{diagnostic_report.power_consumption}" # Expected: 3847100
+  puts 'Part 2: Oxygen Generator Rating * CO2 Scrubber Rating = '\
+       "#{diagnostic_report.oxygen_generator_rating * diagnostic_report.co2_scrubber_rating}" # Expected: 4105235
 end
--- 03/day_three_test.rb
+++ 03/day_three_test.rb
@@ -29,4 +29,31 @@ class DiagnosticReportTest < Minitest::Test
     assert_equal(22, @diagnostic_report.gamma_rate)
     assert_equal(9, @diagnostic_report.epsilon_rate)
   end
+
+  def test_part_two_example
+    @diagnostic_report.parse_entry('00100')
+    @diagnostic_report.parse_entry('11110')
+    @diagnostic_report.parse_entry('10110')
+    @diagnostic_report.parse_entry('10111')
+    @diagnostic_report.parse_entry('10101')
+    @diagnostic_report.parse_entry('01111')
+    @diagnostic_report.parse_entry('00111')
+    @diagnostic_report.parse_entry('11100')
+    @diagnostic_report.parse_entry('10000')
+    @diagnostic_report.parse_entry('11001')
+    @diagnostic_report.parse_entry('00010')
+    @diagnostic_report.parse_entry('01010')
+
+    assert_equal(23, @diagnostic_report.oxygen_generator_rating)
+    assert_equal(10, @diagnostic_report.co2_scrubber_rating)
+  end
+
+  def test_assert_correct_answers
+    File.foreach(File.join(File.dirname(__FILE__), 'day_three_input.txt')) do |line|
+      @diagnostic_report.parse_entry(line.chomp)
+    end
+
+    assert_equal(3847100, @diagnostic_report.power_consumption)
+    assert_equal(4105235, @diagnostic_report.oxygen_generator_rating * @diagnostic_report.co2_scrubber_rating)
+  end
 end
