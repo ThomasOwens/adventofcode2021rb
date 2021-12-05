b23570f29e7c6f49456b8901c3dba6b517c07905
Thomas J. Owens <thomas.j.owens@gmail.com>
2021-12-05 06:55:10 -0500

    Cleaning up

--- 04/day_four.rb
+++ 04/day_four.rb
@@ -1,58 +1,6 @@
 # frozen_string_literal: true
 
-#######################################################################################################################
-# --- Day 4: Giant Squid ---
-#
-# --- Part One ---
-# You're already almost 1.5km (almost a mile) below the surface of the ocean, already so deep that you can't see any
-# sunlight. What you can see, however, is a giant squid that has attached itself to the outside of your submarine.
-#
-# Maybe it wants to play bingo?
-#
-# Bingo is played on a set of boards each consisting of a 5x5 grid of numbers. Numbers are chosen at random, and the
-# chosen number is marked on all boards on which it appears. (Numbers may not appear on all boards.) If all numbers in
-# any row or any column of a board are marked, that board wins. (Diagonals don't count.)
-#
-# The submarine has a bingo subsystem to help passengers (currently, you and the giant squid) pass the time. It
-# automatically generates a random order in which to draw numbers and a random set of boards (your puzzle input). For
-# example:
-#
-# 7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-#
-# 22 13 17 11  0
-#  8  2 23  4 24
-# 21  9 14 16  7
-#  6 10  3 18  5
-#  1 12 20 15 19
-#
-#  3 15  0  2 22
-#  9 18 13 17  5
-# 19  8  7 25 23
-# 20 11 10 24  4
-# 14 21 16 12  6
-#
-# 14 21 17 24  4
-# 10 16 15  9 19
-# 18  8 23 26 20
-# 22 11 13  6  5
-#  2  0 12  3  7
-#
-# After the first five numbers are drawn (7, 4, 9, 5, and 11), there are no winners
-#
-# After the next six numbers are drawn (17, 23, 2, 0, 14, and 21), there are still no winners.
-#
-# Finally, 24 is drawn.
-#
-# At this point, the third board wins because it has at least one complete row or column of marked numbers (in this
-# case, the entire top row is marked: 14 21 17 24 4).
-#
-# The score of the winning board can now be calculated. Start by finding the sum of all unmarked numbers on that board;
-# in this case, the sum is 188. Then, multiply that sum by the number that was just called when the board won, 24, to
-# get the final score, 188 * 24 = 4512.
-#
-# To guarantee victory against the giant squid, figure out which board will win first. What will your final score be if
-# you choose that board?
-#######################################################################################################################
+# https://adventofcode.com/2021/day/4
 
 class Board
   def initialize(board_lines)
@@ -80,7 +28,7 @@ class Board
   end
 
   def remaining_value
-    @board.flatten.compact.sum(&:to_i)
+    @board.flatten.compact.sum
   end
 end
 
@@ -108,7 +56,7 @@ class Game
   end
 
   def score
-    winning_boards[0].remaining_value * @last_called_number.to_i
+    winning_boards[0].remaining_value * @last_called_number
   end
 end
 
@@ -117,11 +65,12 @@ if $PROGRAM_NAME == __FILE__
 
   input_file = File.new(ARGV[0])
 
-  numbers = input_file.readline.strip.split(',')
+  numbers = input_file.readline.strip.split(',').map(&:to_i)
   
   board_blocks = input_file.readlines.map(&:strip).delete_if do |line|
     line.strip.empty?
   end.map(&:split).enum_for(:each_slice, 5).to_a
+  board_blocks.each { |board| board.each { |row| row.map!(&:to_i) } }
 
   boards = []
   board_blocks.each { |board_block| boards.push(Board.new(board_block)) }
--- 04/day_four_test.rb
+++ 04/day_four_test.rb
@@ -5,11 +5,11 @@ require_relative 'day_four'
 
 class BoardTest < MiniTest::Test
   def setup
-    @board = Board.new([%w[22 13 17 11 0],
-                        %w[8 2 23 4 24],
-                        %w[21 9 14 16 7],
-                        %w[6 10 3 18 5],
-                        %w[1 12 20 15 19]])
+    @board = Board.new([[22, 13, 17, 11, 0],
+                        [8, 2, 23, 4, 24],
+                        [21, 9, 14, 16, 7],
+                        [6, 10, 3, 18, 5],
+                        [1, 12, 20, 15, 19]])
   end
 
   def test_can_initialize_board
@@ -17,7 +17,7 @@ class BoardTest < MiniTest::Test
   end
 
   def test_can_mark_number
-    assert_silent { @board.mark('22') }
+    assert_silent { @board.mark(22) }
   end
 
   def test_no_winner_no_marked_numbers
@@ -25,37 +25,37 @@ class BoardTest < MiniTest::Test
   end
 
   def test_no_winner_with_marked_numbers
-    @board.mark('21')
-    @board.mark('9')
-    @board.mark('14')
+    @board.mark(21)
+    @board.mark(9)
+    @board.mark(14)
     refute(@board.winner?)
   end
 
   def test_horizontal_win
-    @board.mark('21')
-    @board.mark('9')
-    @board.mark('14')
-    @board.mark('16')
-    @board.mark('7')
+    @board.mark(21)
+    @board.mark(9)
+    @board.mark(14)
+    @board.mark(16)
+    @board.mark(7)
     assert(@board.winner?)
   end
 
   def test_vertical_win
-    @board.mark('13')
-    @board.mark('2')
-    @board.mark('9')
-    @board.mark('10')
-    @board.mark('12')
+    @board.mark(13)
+    @board.mark(2)
+    @board.mark(9)
+    @board.mark(10)
+    @board.mark(12)
 
     assert(@board.winner?)
   end
 
   def test_remaining_value
-    @board.mark('13')
-    @board.mark('2')
-    @board.mark('9')
-    @board.mark('10')
-    @board.mark('12')
+    @board.mark(13)
+    @board.mark(2)
+    @board.mark(9)
+    @board.mark(10)
+    @board.mark(12)
 
     assert_equal(254, @board.remaining_value)
   end
@@ -63,77 +63,77 @@ end
 
 class GameTest < MiniTest::Test
   def setup
-    @numbers = %w[7 4 9 5 11 17 23 2 0 14 21 24 10 16 13 6 15 25 12 22 18 20
-                  8 19 3 26 1]
+    @numbers = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20,
+                8, 19, 3, 26, 1]
 
-    @board_one = Board.new([%w[22 13 17 11 0],
-                            %w[8 2 23 4 24],
-                            %w[21 9 14 16 7],
-                            %w[6 10 3 18 5],
-                            %w[1 12 20 15 19]])
+    @board_one = Board.new([[22, 13, 17, 11, 0],
+                            [8, 2, 23, 4, 24],
+                            [21, 9, 14, 16, 7],
+                            [6, 10, 3, 18, 5],
+                            [1, 12, 20, 15, 19]])
 
-    @board_two = Board.new([%w[3 15 0 2 22],
-                            %w[9 18 13 17 5],
-                            %w[19 8 7 25 23],
-                            %w[20 11 10 24 4],
-                            %w[14 21 16 12 6]])
+    @board_two = Board.new([[3, 15, 0, 2, 22],
+                            [9, 18, 13, 17, 5],
+                            [19, 8, 7, 25, 23],
+                            [20, 11, 10, 24, 4],
+                            [14, 21, 16, 12, 6]])
 
-    @board_three = Board.new([%w[14 21 17 24 4],
-                              %w[10 16 15 9 19],
-                              %w[18 8 23 26 20],
-                              %w[22 11 13 6 5],
-                              %w[2 0 12 3 7]])
+    @board_three = Board.new([[14, 21, 17, 24, 4],
+                              [10, 16, 15, 9, 19],
+                              [18, 8, 23, 26, 20],
+                              [22, 11, 13, 6, 5],
+                              [2, 0, 12, 3, 7]])
 
     @game = Game.new(@numbers, [@board_one, @board_two, @board_three])
   end
 
   def test_part_one_example
     @game.call_number
-    assert_equal('7', @game.last_called_number)
+    assert_equal(7, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('4', @game.last_called_number)
+    assert_equal(4, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('9', @game.last_called_number)
+    assert_equal(9, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('5', @game.last_called_number)
+    assert_equal(5, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('11', @game.last_called_number)
+    assert_equal(11, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('17', @game.last_called_number)
+    assert_equal(17, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('23', @game.last_called_number)
+    assert_equal(23, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('2', @game.last_called_number)
+    assert_equal(2, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('0', @game.last_called_number)
+    assert_equal(0, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('14', @game.last_called_number)
+    assert_equal(14, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('21', @game.last_called_number)
+    assert_equal(21, @game.last_called_number)
     refute(@game.winning_boards?)
 
     @game.call_number
-    assert_equal('24', @game.last_called_number)
+    assert_equal(24, @game.last_called_number)
     assert(@game.winning_boards?)
 
     assert_equal(188, @game.winning_boards[0].remaining_value)
