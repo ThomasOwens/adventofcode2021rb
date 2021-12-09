0082f09b07d0bd13f827a2a1692cc9af4b52625c
Thomas J. Owens <thomas.j.owens@gmail.com>
2021-12-08 20:06:03 -0500

    Day 8 Part 2 Answered

--- 08/day_eight.rb
+++ 08/day_eight.rb
@@ -3,65 +3,60 @@
 # https://adventofcode.com/2021/day/8
 
 class SevenSegmentDisplay
-  def initialize
-    @display = {}
-    @digits = {}
+  def initialize(signal_patterns, output_value)
+    @input = signal_patterns
+    @output = output_value.map { |element| element.chars.sort }
   end
 
-  def add_entry(signal_patterns, output_value)
-    raise IllegalArgumentException, 'incorrect number of signal patterns' unless signal_patterns.length == 10
-    raise IllegalArgumentException, 'incorrect output value' unless output_value.length == 4
+  def mapping
+    return @mapping if @mapping
 
-    @display[signal_patterns] = output_value
+    @mapping = Array.new(10, 0)
+    @mapping[1] = @input.find { |pattern| pattern.length == 2 }.chars.sort
+    @mapping[4] = @input.find { |pattern| pattern.length == 4 }.chars.sort
+    @mapping[7] = @input.find { |pattern| pattern.length == 3 }.chars.sort
+    @mapping[8] = @input.find { |pattern| pattern.length == 7 }.chars.sort
+    @mapping[3] = @input.find do |pattern|
+      pattern.length == 5 && (pattern.chars.sort - mapping[1]).length == 3
+    end.chars.sort
+    @mapping[5] = @input.find do |pattern|
+      pattern.length == 5 && (pattern.chars.sort - mapping[1]).length == 4 &&
+        (pattern.chars.sort - mapping[4]).length == 2
+    end.chars.sort
+    @mapping[2] = @input.find do |pattern|
+      pattern.length == 5 && (pattern.chars.sort - mapping[4]).length == 3
+    end.chars.sort
+    @mapping[6] = @input.find do |pattern|
+      pattern.length == 6 && (pattern.chars.sort - mapping[1]).length == 5
+    end.chars.sort
+    @mapping[9] = @input.find do |pattern|
+      pattern.length == 6 && (pattern.chars.sort - mapping[3]).length == 1
+    end.chars.sort
+    @mapping[0] = @input.find do |pattern|
+      pattern.length == 6 && (pattern.chars.sort - mapping[6]).length == 1 &&
+        (pattern.chars.sort - mapping[9]).length == 1
+    end.chars.sort
+    @mapping
   end
 
-  def signal_patterns
-    @display.keys
+  def output_value
+    [mapping.index(@output[0]), mapping.index(@output[1]), mapping.index(@output[2]), 
+     mapping.index(@output[3])].join.to_i
   end
-
-  def output_values
-    @display.values
-  end
-
-  def output_value_digits
-    digits = Array.new(10, 0)
-
-    output_values.each do |output_value| 
-      output_value.each do |output_value_digit|
-        digits[output_value_to_digit(output_value_digit)] += 1
-      end                                        
-    end    
-
-    digits
-  end
-
-  private
-
-    def output_value_to_digit(output_value)
-      return 1 if output_value.length == 2
-      return 4 if output_value.length == 4
-      return 7 if output_value.length == 3
-      return 8 if output_value.length == 7
-
-      0
-    end
 end
 
 if $PROGRAM_NAME == __FILE__
   raise ArgumentError, 'Missing or invalid input file.' unless File.file?(ARGV[0]) && File.readable?(ARGV[0])
 
+  sum_of_output_values = 0
+
   input_file = File.new(ARGV[0])
-
-  display = SevenSegmentDisplay.new
-
   entries = input_file.readlines
   entries.each do |entry|
     split_entries = entry.split('|')
-    display.add_entry(split_entries[0].strip.split, split_entries[1].strip.split)
+    display = SevenSegmentDisplay.new(split_entries[0].strip.split, split_entries[1].strip.split)
+    sum_of_output_values += display.output_value
   end
 
-  # Part 1
-  num_digits = display.output_value_digits[1] + display.output_value_digits[4] +
-               display.output_value_digits[7] + display.output_value_digits[8]
-  pp "Digits with unique number of segments: #{num_digits}" # Expected: 344
+  pp "Sum of Output Values: #{sum_of_output_values}"
 end
--- 08/day_eight_test.rb
+++ 08/day_eight_test.rb
@@ -4,58 +4,57 @@ require 'minitest/autorun'
 require_relative 'day_eight'
 
 class SevenSegmentDisplayTest < MiniTest::Test
-  def setup
-    @display = SevenSegmentDisplay.new
+  def test_part_two_example_one
+    display = SevenSegmentDisplay.new(%w[acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab], 
+                                      %w[cdfeb fcadb cdfeb cdbaf])
+    
+    assert_equal(%w[c a g e d b].sort, display.mapping[0])
+    assert_equal(%w[a b].sort, display.mapping[1])
+    assert_equal(%w[g c d f a].sort, display.mapping[2])
+    assert_equal(%w[f b c a d].sort, display.mapping[3])
+    assert_equal(%w[e a f b].sort, display.mapping[4])
+    assert_equal(%w[c d f b e].sort, display.mapping[5])
+    assert_equal(%w[c d f g e b].sort, display.mapping[6])
+    assert_equal(%w[d a b].sort, display.mapping[7])
+    assert_equal(%w[a c e d g f b].sort, display.mapping[8])
+    assert_equal(%w[c e f a b d].sort, display.mapping[9])
+
+    assert_equal(5353, display.output_value)
   end
 
-  def test_signal_patterns
-    test_signal_patterns = %w[acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab]
-    test_output_value = %w[cdfeb fcadb cdfeb cdbaf]
-    @display.add_entry(test_signal_patterns, test_output_value)
+  def test_part_two_example_two
+    output1 = SevenSegmentDisplay.new(%w[be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb], 
+                                      %w[fdgacbe cefdb cefbgd gcbe]).output_value
+    output2 = SevenSegmentDisplay.new(%w[edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec], 
+                                      %w[fcgedb cgb dgebacf gc]).output_value
+    output3 = SevenSegmentDisplay.new(%w[fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef], 
+                                      %w[cg cg fdcagb cbg]).output_value
+    output4 = SevenSegmentDisplay.new(%w[fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega], 
+                                      %w[efabcd cedba gadfec cb]).output_value
+    output5 = SevenSegmentDisplay.new(%w[aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga], 
+                                      %w[gecf egdcabf bgf bfgea]).output_value
+    output6 = SevenSegmentDisplay.new(%w[fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf], 
+                                      %w[gebdcfa ecba ca fadegcb]).output_value
+    output7 = SevenSegmentDisplay.new(%w[dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf], 
+                                      %w[cefg dcbef fcge gbcadfe]).output_value
+    output8 = SevenSegmentDisplay.new(%w[bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd], 
+                                      %w[ed bcgafe cdgba cbgef]).output_value
+    output9 = SevenSegmentDisplay.new(%w[egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg], 
+                                      %w[gbdfcae bgc cg cgb]).output_value
+    output0 = SevenSegmentDisplay.new(%w[gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc], 
+                                      %w[fgae cfgab fg bagce]).output_value
 
-    assert_equal(1, @display.signal_patterns.length)
-    assert(@display.signal_patterns.member?(test_signal_patterns))
-  end
-
-  def test_output_value
-    test_signal_patterns = %w[acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab]
-    test_output_value = %w[cdfeb fcadb cdfeb cdbaf]
-    @display.add_entry(test_signal_patterns, test_output_value)
-  
-    assert_equal(1, @display.output_values.length)
-    assert(@display.output_values.member?(test_output_value))
-  end
-
-  def test_part_one_example
-    test_signal_patterns = []
-    test_signal_patterns.append(%w[be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb])
-    test_signal_patterns.append(%w[edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec])
-    test_signal_patterns.append(%w[fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef])
-    test_signal_patterns.append(%w[fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega])
-    test_signal_patterns.append(%w[aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga])
-    test_signal_patterns.append(%w[fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf])
-    test_signal_patterns.append(%w[dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf])
-    test_signal_patterns.append(%w[bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd])
-    test_signal_patterns.append(%w[egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg])
-    test_signal_patterns.append(%w[gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc])
-
-    test_output_values = []
-    test_output_values.append(%w[fdgacbe cefdb cefbgd gcbe])
-    test_output_values.append(%w[fcgedb cgb dgebacf gc])
-    test_output_values.append(%w[cg cg fdcagb cbg])
-    test_output_values.append(%w[efabcd cedba gadfec cb])
-    test_output_values.append(%w[gecf egdcabf bgf bfgea])
-    test_output_values.append(%w[gebdcfa ecba ca fadegcb])
-    test_output_values.append(%w[cefg dcbef fcge gbcadfe])
-    test_output_values.append(%w[ed bcgafe cdgba cbgef])
-    test_output_values.append(%w[gbdfcae bgc cg cgb])
-    test_output_values.append(%w[fgae cfgab fg bagce])
-
-    (0..test_signal_patterns.length - 1).each do |index|
-      @display.add_entry(test_signal_patterns[index], test_output_values[index])
-    end    
-
-    output_digits = @display.output_value_digits
-    assert_equal(26, output_digits[1] + output_digits[4] + output_digits[7] + output_digits[8])
+    assert_equal(8394, output1)
+    assert_equal(9781, output2)
+    assert_equal(1197, output3)
+    assert_equal(9361, output4)
+    assert_equal(4873, output5)
+    assert_equal(8418, output6)
+    assert_equal(4548, output7)
+    assert_equal(1625, output8)
+    assert_equal(8717, output9)
+    assert_equal(4315, output0)
+    assert_equal(61229, 
+                 output1 + output2 + output3 + output4 + output5 + output6 + output7 + output8 + output9 + output0)
   end
 end
